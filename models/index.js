c4779da5c93448550b59bf23240390677fe78539
John Ferlito <johnferlito73@gmail.com>
2024-10-31 22:20:38 -0400

    finish models

--- models/index.js
+++ models/index.js
@@ -4,13 +4,34 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// Categories have many Products
+
+Category.hasMany(Product,{
+  foreignKey:'category_id',
+  onDelete:'CASCADE'
+})
 // Products belongsTo Category
 
-// Categories have many Products
+Product.belongsTo(Category,{
+  foreignKey:'category_id'
+})
+
+
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag,{
+  through:{
+    model: ProductTag
+  }
+})
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product,{
+  through:{
+    model: ProductTag
+  }
+})
 
 module.exports = {
   Product,
